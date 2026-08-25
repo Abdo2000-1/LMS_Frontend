d7c1bad93b417db98baf733e4ca15ba43a250e8a
Abdo Al Adawy <abdoaladawy2000@gmail.com>
2026-08-25 23:41:59 +0300

    SEO

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
-  const base = env.VITE_BASE_PATH ?? (mode === "production" ? "/" : "/LMS_Frontend/");
+
+  const base = env.VITE_BASE_PATH ?? (mode === "production" ? "/LMS/" : "/");
 
   return {
     plugins: [react()],
